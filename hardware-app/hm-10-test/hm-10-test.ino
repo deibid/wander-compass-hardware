4c272ea4bf8046afe620f3fff87f9a45882d31c6
deibid <davidazar3@gmail.com>
2019-05-05 00:17:27 -0400

    initial commit on separate repo

---
+++ hardware-app/hm-10-test/hm-10-test.ino
@@ -0,0 +1,16 @@
+#include "SoftwareSerial/SoftwareSerial.h"
+
+
+SoftwareSerial mySerial(3,4);
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
